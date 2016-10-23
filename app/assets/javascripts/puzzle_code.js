cf8d218824c342a6bfe489b257dd3ba082a277de
Matt Larrabee <mattlarrabee@gmail.com>
2016-10-23 14:37:31 -0400

    multi animate progresS

--- app/assets/javascripts/puzzle_code.js
+++ app/assets/javascripts/puzzle_code.js
@@ -9,6 +9,8 @@ var correct = 0;
 
     function runCode() {
 
+
+
      
 
     	var rootBlock = null;
@@ -20,9 +22,9 @@ var correct = 0;
       var rootCoord = block.getRelativeToSurfaceXY();
       
     }
-    else if (block.type == 'get_input') {
+    /*else if (block.type == 'get_input') {
       var coord = block.getRelativeToSurfaceXY();
-    }
+    }*/
   }
   var output = NaN;
   Blockly.JavaScript.init(workspace);
@@ -36,7 +38,7 @@ var correct = 0;
 
   
 
-      document.getElementById("coord").innerHTML = coord.x + " " + coord.y;     
+      //document.getElementById("coord").innerHTML = coord.x + " " + coord.y;     
     
       
 
@@ -55,23 +57,104 @@ for (var i = 0, len = INPUT.input_array.length; i < len; i++) {
       }
           var output = 0;
     output = eval(code);
+    OUTPUT.output_array[i] = output;
     //document.getElementById("output").innerHTML = output; 
 
-        $("#input-list" ).animate({ "left": "+="+(coord.x + 100), "top": "+="+(coord.y+40) }, "slow", function() {
+    
 
-        $( "#input-list" ).fadeOut("slow", function () {
-           document.getElementById("input-list").innerHTML = output; 
+    
+    
 
-            $("#input-list" ).animate({ "left": "-="+(coord.x + 100), "top": "-="+(coord.y+40) }, 0, function() {
+    document.getElementById("output" + (i+1)).innerHTML = output; 
+    if (output === correct) {
+    	document.getElementById("grade" + (i+1)).innerHTML = "correct!"; 
+    };
+  };
 
-              $("#input-list" ).animate({ "left": "+="+(rootCoord.x + 130), "top": "+="+(rootCoord.y+45) }, 0, function() {
+    //need to make this a choice, need to also delay painting of table, and also dynamically build table.
+    runAnimation();
+
+};
+
+function displayInputs() {
+
+   var inputDiv = document.getElementById("inputs");
+
+
+for (var i = 0, len = INPUT.input_array.length; i < len; i++) {
+    var comma =    document.createElement('span');
+  comma.innerHTML = ", "
+  var g = document.createElement('span');
+  g.id = 'input-list' + i;
+  
+  g.innerHTML = INPUT.input_array[i]
+  var g2 = inputDiv.appendChild(g);
+  g2.classname = 'input';
+  
+  if (i != len - 1 ) {
+  inputDiv.appendChild(comma);
+}
+  
+  }
+
+}
+
+function runAnimation() {
+
+    var blocks = workspace.getAllBlocks();
+  for (var i = 0, block; block = blocks[i]; i++) {
+    if (block.type == 'answer') {
+      rootCoord = block.getRelativeToSurfaceXY();
+    }
+   
+    else if (block.type == 'get_input') {
+      var coord = block.getRelativeToSurfaceXY();
+    }
+  }
+   
+  var count = 0;
+    //while (count < INPUT.input_array.length) {
+    function animationLoop(count) {
+
+
+        //need to offset by count, to adjust for locations of input array spans
+        $("#input-list" + count).animate({ "left": "+="+(coord.x + 100), "top": "+="+(coord.y+40) }, "slow", function() {
+
+        $( "#input-list" + count).fadeOut("slow", function () {
+           document.getElementById("input-list" + count).innerHTML = OUTPUT.output_array[count]; 
+           if (OUTPUT.output_array[count] == CORRECT_OUTPUT.output_array[count]) {
+            $("#input-list" + count).css('color', 'green');  //right or wrong? green or red?
+           } else {
+            $("#input-list" + count).css('color', 'red');
+           }
+
+
+            $("#input-list" + count).animate({ "left": "-="+(coord.x + 100), "top": "-="+(coord.y+40) }, 0, function() {
+
+              $("#input-list" + count).animate({ "left": "+="+(rootCoord.x + 120), "top": "+="+(rootCoord.y+45) }, 0, function() {
                   
-                  $( "#input-list" ).fadeIn({queue: false, duration: "fast"});
+                  $( "#input-list" + count).fadeIn({queue: true, duration: "fast"});
 
-                      $("#input-list" ).animate({"top": "-=20"}, "slow", function() {
+                      $("#input-list" + count).animate({"top": "-=20"}, "slow", function() {
 
-                      //});
+                            $( "#input-list" + count).fadeOut("slow", function () {
 
+
+                          $( "#input-list" + count).remove();
+                          $('#inputs span').first().remove();
+                          if (count < (INPUT.input_array.length - 1)) {
+                          count++;
+                          animationLoop(count);
+                          
+                          } else {
+                               displayInputs();
+
+
+                          }
+
+                        });
+
+                  
 
 
                       });
@@ -81,14 +164,15 @@ for (var i = 0, len = INPUT.input_array.length; i < len; i++) {
                 });  
         });
     });   
+                   
+  }
+
+                     animationLoop(count);
+                    
+
+
    
-    
-    document.getElementById("output" + (i+1)).innerHTML = output; 
-    if (output === correct) {
-    	document.getElementById("grade" + (i+1)).innerHTML = "correct!"; 
-    };
-  };
-};
+}
 
   
 
