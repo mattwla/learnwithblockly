d6fe233c1b517b2526729a1532fb848114165662
Matt Larrabee <mattlarrabee@gmail.com>
2016-11-29 19:28:41 -0500

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root 'main_pages#home'
 
   get 'portfolio/larrabee'
+
+   get    'portfolio'   => 'portfolio#larrabee'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
